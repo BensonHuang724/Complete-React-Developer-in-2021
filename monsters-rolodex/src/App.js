181a887728fb40c6164dd935d6011330a8bcfe01
Hsuan-Hsueh Huang <ben0630882@gmail.com>
2021-04-05 12:55:46 +0800

    finish all components(search-bar card-list and card)

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import {CardList} from './components/card-list/card-list.component'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './components/search-box/search-box.styles.css'
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      monsters: [],
+      pattern: '',
+      filtered_monsters:[],
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({monsters:users,filtered_monsters:users})});
+  }
+  handleChange = (e) => {
+    var pattern = e.target.value
+    var filtered_monsters = this.state.monsters.filter(monster => monster.name.includes(pattern))
+    this.setState( { pattern           : pattern, 
+                     filtered_monsters : filtered_monsters,
+                    })
+  }
+  render(){
+    console.log(this.state)
+    return (
+      <div className="App">
+          <h1> Monsters Rolodex </h1>
+          <input className="search" type="text" placeholder="Search..." onInput= {this.handleChange}/> 
+          <CardList monsters = {this.state.filtered_monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
